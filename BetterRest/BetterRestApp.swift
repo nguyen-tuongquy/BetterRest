2e5004be2572b07f6badcae167b14c4d7bdd4484
Nguyen Tuong Quy <tuongquynguyen.159@gmail.com>
2021-11-29 05:45:01 -0800

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by KET on 29/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
