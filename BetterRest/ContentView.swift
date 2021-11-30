6ec7f3f08d630307e0a71c00bf9bc76a4855ce76
Nguyen Tuong Quy <tuongquynguyen.159@gmail.com>
2021-11-30 05:54:51 -0800

    Basic layout and connect to model

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -5,12 +5,51 @@
 //  Created by KET on 29/11/2021.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                
+                
+            }
+            .navigationTitle("Better Rest")
+            .toolbar {
+                Button("Calculate", action: caculateBedtime)
+            }
+        }
+    }
+}
+
+func caculateBedtime() {
+    do {
+        let config = MLModelConfiguration()
+        let model = try SleepCalculator(configuration: config)
+        
+        //do something
+    } catch {
+        //something went wrong
     }
 }
 
