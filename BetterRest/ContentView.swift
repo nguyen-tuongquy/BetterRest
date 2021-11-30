e17e150258834e8d1cfd7d74df0f4aabc2200486
Nguyen Tuong Quy <tuongquynguyen.159@gmail.com>
2021-11-30 06:41:33 -0800

    finish the application

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -10,27 +10,43 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var wakeUp = Date.now
+    @State private var wakeUp = defaultWakeup
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    static var defaultWakeup: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
     
     var body: some View {
         NavigationView {
-            VStack {
-                Text("When do you want to wake up?")
-                    .font(.headline)
+            Form {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                    
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
                 
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
                 
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-                
-                Text("Daily coffee intake")
-                    .font(.headline)
-                
-                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    
+                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                }
                 
                 
             }
@@ -38,20 +54,41 @@ struct ContentView: View {
             .toolbar {
                 Button("Calculate", action: caculateBedtime)
             }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") {}
+            } message: {
+                Text(alertMessage)
+            }
         }
     }
+    
+    func caculateBedtime() {
+        do {
+//            create ML Model and instance
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+//            get datecomponent for calculating
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+
+//            get actualSleep after prediction
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let idealBedtime = wakeUp - prediction.actualSleep
+            
+            alertTitle = "Your ideal bedtime is:"
+            alertMessage = idealBedtime.formatted(date: .omitted, time: .shortened)
+            
+        } catch {
+            alertTitle = "Error!"
+            alertMessage = "There was a problem calculating your bedtime"
+        }
+        showingAlert = true
+    }
+    
 }
 
-func caculateBedtime() {
-    do {
-        let config = MLModelConfiguration()
-        let model = try SleepCalculator(configuration: config)
-        
-        //do something
-    } catch {
-        //something went wrong
-    }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
